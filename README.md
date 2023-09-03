57b8258a79ac156b9c5b08c9d933aa4269eb0fe4
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-09-03 17:55:44 +0500

    Main File of CRUD API

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Crud-API-Golang
+# CRUD Api Develope using Gogin Golang Framework
+
+This API has all main features like Get a SingleUser and  All User, Delete One User base on a User Name, Update the User as a like User name
+
+
+## Funcationality
+
+ - GET All User
+ - GET Single User
+ - POST Create New User
+ - PUT Update the Existing User
+ - DELETE the Existing User
+
+## Authors
+
+- [@nomankhokhar](https://www.github.com/nomankhokhar)
+
+## Badges
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+## 🚀 About Me
+
+I'm a full Stack Developer...
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"example.com/noman-ali/controllers"
+	"example.com/noman-ali/services"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server      *gin.Engine
+	us          services.UserService
+	uc          controllers.UserController
+	ctx         context.Context
+	userc       *mongo.Collection
+	mongoclient *mongo.Client
+	err         error
+)
+
+func init() {
+	ctx = context.TODO()
+
+	mongoconn := options.Client().ApplyURI("mongodb+srv://nextjs13:nextjs13@cluster0.v4qjg7w.mongodb.net/?retryWrites=true&w=majority")
+
+	mongoclient, err = mongo.Connect(ctx, mongoconn)
+
+	if err != nil {
+		log.Fatal("err while connecting with MongoDB Atlas", err)
+	}
+
+	err = mongoclient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal("Error while trying to ping MongoDB Atlas", err)
+	}
+
+	fmt.Println("MongoDB Atlas connection established")
+
+	// Update the database and collection names as needed.
+	userc = mongoclient.Database("<database_name>").Collection("users")
+	us = services.NewUserService(userc, ctx)
+	uc = controllers.New(us)
+	server = gin.Default()
+}
+
+func main() {
+	defer mongoclient.Disconnect(ctx)
+
+	basepath := server.Group("/v1")
+	uc.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
+}
