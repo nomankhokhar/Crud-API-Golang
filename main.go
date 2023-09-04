a8c8576a38b33324f08ed8f3e517e7c5af56ffaa
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-09-04 23:45:57 +0500

    UPdate the Golang Cde

--- main.go
+++ main.go
@@ -14,22 +14,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-var (
-	server      *gin.Engine
-	us          services.UserService
-	uc          controllers.UserController
-	ctx         context.Context
-	userc       *mongo.Collection
-	mongoclient *mongo.Client
-	err         error
-)
-
 func init() {
-	ctx = context.TODO()
+	ctx := context.TODO()
 
 	mongoconn := options.Client().ApplyURI("mongodb+srv://nextjs13:nextjs13@cluster0.v4qjg7w.mongodb.net/?retryWrites=true&w=majority")
 
-	mongoclient, err = mongo.Connect(ctx, mongoconn)
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
 
 	if err != nil {
 		log.Fatal("err while connecting with MongoDB Atlas", err)
@@ -43,22 +33,23 @@ func init() {
 	fmt.Println("MongoDB Atlas connection established")
 
 	// Update the database and collection names as needed.
-	userc = mongoclient.Database("<database_name>").Collection("users")
-	us = services.NewUserService(userc, ctx)
-	uc = controllers.New(us)
-	server = gin.Default()
+	userc := mongoclient.Database("<database_name>").Collection("users")
+	us := services.NewUserService(userc, ctx)
+	uc := controllers.New(us)
+	server := gin.Default()
 	server.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:8080"},
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders: []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
 	}))
-}
 
-func main() {
 	defer mongoclient.Disconnect(ctx)
 
 	basepath := server.Group("/v1")
 	uc.RegisterUserRoutes(basepath)
 
 	log.Fatal(server.Run(":9090"))
+}
+func main() {
+
 }
